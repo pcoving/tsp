15fa83d48471203985b4efc6efd016f63851963c
pcoving <pmcovington@gmail.com>
2012-12-20 11:46:59 -0800

    reverted to alternating greedy - no obvious benefit for adding to shortest path

--- scripts/route.py
+++ scripts/route.py
@@ -6,19 +6,16 @@ from tools import city_dist
 def NN_wrapper():
     
     cities=io.load_cities()
-    start = np.zeros(2)
-
-    start_min = start
+    
+    start_min = None
     score_min = 1e20
     while (1):
-        start[0] = np.random.randint(0, 150000)
-        start[1] = np.random.randint(0, 150000)
-        if (start[0] != start[1]):
-            route, score = NN(cities=cities, start=start)
+        start = np.random.randint(low=0, high=150000, size=2)
+        route, score = NN(cities=cities, start=start)
         if (score < score_min):
             score_min = score
             start_min = start
-            print score_min, start_min
+            print "score_min, start_min: ", score_min, start_min
 
 def NN(cities=io.load_cities(), start=np.array([0,0])):
     '''
@@ -59,57 +56,39 @@ def NN(cities=io.load_cities(), start=np.array([0,0])):
     dist = np.array([0.0, 0.0])
     
     tree = cKDTree(cities)
-    ir = 0
-    ic = np.array([0,0], dtype=int)
-    while ((ic[0] < Nc-1) | (ic[1] < Nc-1)):
-        #print ir, ic[0], ic[1]
-        #if (ic[0]%10000 == 0):
-        #    print "working on city: ", ic[0], int(np.max(dist))
-
-        ind = ic[ir]
-        if (~(ind < Nc-1)):
-            print "here: ", ir, ic
-        thiscity = myroute[ir][ind] 
-        
-        assert(edges[ir][thiscity, 0] >= 0)
-        assert(edges[ir][thiscity, 1] == -1)
-        
-        knbrs = 10
-        nextcity = -1
-        while(nextcity == -1):
-            nbrs_dist, nbrs = tree.query(cities[thiscity], knbrs)
-            
-            for nbr in nbrs:
-                if (nbr == Nc):
-                    print "failed to find next city"
-                    print ir, ic, nbrs_dist, nbrs
-                    return myroute
-                # check if we've already been here...
-                if (edges[ir][nbr,0] == -1):
-                    # eliminate edges in other route...
-                    if ((edges[(ir+1)%2][thiscity, 0] != nbr) &
-                        (edges[(ir+1)%2][thiscity, 1] != nbr)):
-                        nextcity = nbr
-                        break
+    for ic in xrange(Nc-1):
+        for ir in range(2):
+            #if (ic%10000 == 0):
+            #    print "working on city: ", ic, int(np.max(dist))
                 
-            knbrs *= 2
-        
-        myroute[ir][ind+1] = nextcity
-        dist[ir] += city_dist(cities, thiscity, nextcity)
-        edges[ir][thiscity, 1] = nextcity
-        edges[ir][nextcity, 0] = thiscity
-        
-        ic[ir] += 1
-        
-        ir = (ir+1)%2
-        '''
-        if (ic[ir] == Nc-1):
-            ir = (ir+1)%2    # we're done here
-        else:
-            if ((dist[ir] > dist[(ir+1)%2]) & (ic[(ir+1)%2] < Nc-1)):
-                ir = (ir+1)%2
-        ''' 
-    
+            thiscity = myroute[ir][ic] 
+            
+            assert(edges[ir][thiscity, 0] >= 0)
+            assert(edges[ir][thiscity, 1] == -1)
+            
+            knbrs = 10
+            nextcity = -1
+            while(nextcity == -1):
+                nbrs_dist, nbrs = tree.query(cities[thiscity], knbrs)
+                for nbr in nbrs:
+                    if (nbr == Nc):
+                        print "failed to find next city"
+                        return
+                    # check if we've already been here...
+                    if (edges[ir][nbr,0] == -1):
+                        # eliminate edges in other route...
+                        if ((edges[(ir+1)%2][thiscity, 0] != nbr) &
+                            (edges[(ir+1)%2][thiscity, 1] != nbr)):
+                            nextcity = nbr
+                            break
+                
+                knbrs *= 2
+                        
+            myroute[ir][ic+1] = nextcity
+            dist[ir] += city_dist(cities, thiscity, nextcity)
+            edges[ir][thiscity, 1] = nextcity
+            edges[ir][nextcity, 0] = thiscity
+                
     score = int(np.max(dist))
     print "start, score: ", start, score
     return myroute, score
