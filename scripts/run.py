48bf598f9cde328a6ee7ed9180add227f333a4ba
Paul Covington <pcoving@stanford.edu>
2013-01-07 21:07:03 -0800

    latest

--- scripts/run.py
+++ scripts/run.py
@@ -3,7 +3,7 @@ import innout as io
 import route
 
 cities = io.load_cities()
-rr = io.load_route('fromlkh.csv')
-#rr, sc = route.greedyNN(cities, start=np.array([137433, 145655]))
-#io.write_route(route=rr, name='optstart2')
-rr_new = route.opt2(cities, route=rr, look_ahead=20000, name='fromlkh', checkpoint=250)
+rr = io.load_route('optstart_7479503.csv')
+#rr, sc = route.NN(cities, start=np.array([137433, 145655]))
+#io.write_route(route=rr, name='optstart')
+rr_new = route.opt2(cities, route=rr, look_ahead=20000, name='optstart', checkpoint=250)
